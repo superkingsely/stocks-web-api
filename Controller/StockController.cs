01ed38b29356504099d0ad8b70f33b33ce397dbb
chijioke <superkingsely@gmail.com>
2024-06-28 03:20:20 -0700

    okay db connected

---
+++ Controller/StockController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+
+namespace api;
+[Route("api/stock")]
+[ApiController]
+public class StockController:ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll(){
+        var stocks=new List<string>{
+           "ok","too bad ","hmm"
+        };
+        return Ok(stocks);
+    }
+}
---
+++ entity/ApplicationDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api;
+
+public class ApplicationDb:DbContext
+{
+    public ApplicationDb(DbContextOptions contextOptions):base(contextOptions)
+    {
+        
+    }
+    public DbSet<Stock>stocks{get;set;}
+    public DbSet<Comment>Comments{get;set;}
+}
