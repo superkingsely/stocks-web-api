9a18b319395431bfe3149dd0a62157058ec52636
chijioke <superkingsely@gmail.com>
2024-07-08 07:15:00 -0700

    added the identities

--- Program.cs
+++ Program.cs
@@ -27,6 +27,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.MapControllers();
+// vscode =dotnet watch run
 app.Run();
 
 
--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64fcd78699e7c335cffa79f0c11aac2e7f970eea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75b19a84e8c921596b16f5fd020c4c54770fe450")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
